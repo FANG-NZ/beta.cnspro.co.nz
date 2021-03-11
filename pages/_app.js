b0ea40830e8498e0a56f1040d50c2fd41c95c69f
Fang (MacPro) <faang1980@gmail.com>
2021-03-11 16:41:25 +1300

    start working on page

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../assets/vendor/bootstrap-4.1/bootstrap.min.css'
+import '../assets/vendor/css-hamburgers/hamburgers.css'
+import '../styles/main.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/about-us.js
@@ -0,0 +1,9 @@
+
+export default function AboutUsPage(){
+
+    return(
+        <>
+            <h1>TEST About Us Page</h1>
+        </>
+    )
+}
