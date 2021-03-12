52c6e4861370a68f11527a1187a97239f6251eea
Fang (MacPro) <faang1980@gmail.com>
2021-03-13 11:40:51 +1300

    add slider

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 
-//import styles from '../styles/Home.module.css'
+//import $ from "../assets/vendor/jquery/jquery.min.js"
+
+
+const DynamicSliderSection = dynamic(
+  () => import('../components/slider-section'),
+  { ssr: false }
+);
+
+
 
 export default function HomePage() {
   return (
@@ -12,7 +21,19 @@ export default function HomePage() {
         <title>Home - CNSPRO</title>
       </Head>
 
-      <h1>TEST LINE</h1>
+      {/* START page body */}
+      <div className="page-line">
+          <div className="container">
+              <div className="page-line__inner">
+                  <div className="page-col"></div>
+                  <div className="page-col"></div>
+                  <div className="page-col"></div>
+              </div>
+          </div>
+      </div>
+
+      <DynamicSliderSection />
+
     </React.Fragment>
   )
 }
